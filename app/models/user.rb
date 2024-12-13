4981b2c2d123a648ca7d20a2a78f5b0ffea591b1
Kazane Tani <kzn.tani24@gmail.com>
2024-12-13 14:19:38 +0900

    add: Usersテーブルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true, length: { maximum: 20 }
+  validates :last_name, presence: true, length: { maximum: 20 }
+  validates :first_name_kana, presence:true, length: { maximum: 20 }
+  validates :last_name_kana, presence: true, length: { maximum: 20 }
+  validates :gender, presence: true
+end
