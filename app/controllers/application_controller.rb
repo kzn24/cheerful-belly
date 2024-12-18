9720f4b48d384b315b1da1ef01b8a8795cfe85ab
Kazane Tani <kzn.tani24@gmail.com>
2024-12-18 20:05:17 +0900

    improve: Deviseの認証機能(before_action :authenticate_user!)の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller? # deviseで情報登録をするとき、以下の configure_permitted_parameters が適用される
+  before_action :authenticate_user!
 
   protected
 
