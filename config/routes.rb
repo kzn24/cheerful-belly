7e8e1f22366d4d9277fb860e6e7d5663350fd0c9
Kazane Tani <kzn.tani24@gmail.com>
2024-12-28 14:30:28 +0900

    fix: ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,27 @@
 Rails.application.routes.draw do
-  resources :symptoms
-  # Reveal health status on /up
+  # アプリケーションが正常に動作しているかどうかを確認するためのルーティング
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
+  # PWAに必要なサービスワーカーを提供するためのルーティング
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # PWAのマニフェストファイルを提供するためのルーティング
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   root to: "home#index"
 
-  get "main/index", to: "main#index", as: "main_index" # トップからログインした際のルーティング
-  get "home/index", to: "home#index", as: "home_index" # ログイン失敗後トップページに遷移
-  get "drug_supplement_lists/index" # お薬とサプリリストへ
-
-  resources :drug_supplement_lists, only: [ :index, :create ]
-  resources :drugs, only: [ :edit, :update, :destroy ]
-  resources :supplements, only: [ :edit, :update, :destroy ]
-  resources :symptoms
-  # ログアウト後トップページに遷移
+  devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
+  
   devise_scope :user do
     get "/users/sign_out" => "devise/sessions#destroy"
   end
 
-  devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
+  get "main/index", to: "main#index", as: "main_index" # トップからログインした際のルーティング
+  get "home/index", to: "home#index", as: "home_index" # ログイン失敗後トップページに遷移
+
+  resources :users do
+    resources :drug_supplement_lists, only: [ :index, :create ]
+    resources :drugs, only: [ :edit, :update, :destroy ]
+    resources :supplements, only: [ :edit, :update, :destroy ]
+    resources :symptoms, only: [ :index, :create, :edit, :update, :destroy ]
+    resources :schedules
+  end
 end
