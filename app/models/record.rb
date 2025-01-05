10c6e4551b12062630377dcd6347b0b9a6022acd
Kazane Tani <kzn.tani24@gmail.com>
2025-01-05 13:46:48 +0900

    add: Recordモデルにバリデーション追加

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,5 @@
 class Record < ApplicationRecord
   belongs_to :user
 
-  validates :condition_rating, :belly_rating, :meal_rating, :poop_rating, inclusion: { in: 1..5 }
+  validates :condition_rating, :belly_rating, :meal_rating, :poop_rating, inclusion: { in: 0..5 }
 end
