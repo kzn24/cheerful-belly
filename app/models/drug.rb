b8ec2109a2f8adceb035a1fc37770372433885bf
Kazane Tani <kzn.tani24@gmail.com>
2025-02-12 17:14:17 +0900

    fix: 薬が削除されると関連するスケジュールも削除されるように修正

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,6 +1,6 @@
 class Drug < ApplicationRecord
   belongs_to :user
-  has_many :schedules
+  has_many :schedules, dependent: :destroy
 
   validates :drug_name, presence: true
   validates :drug_name, uniqueness: { scope: :user_id }
