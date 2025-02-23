14e232f56a516cc21ef1be706814a47dbf6d52cd
Kazane Tani <kzn.tani24@gmail.com>
2025-02-23 18:50:50 +0900

    add: アソシエーションの設定

--- app/models/supplement.rb
+++ app/models/supplement.rb
@@ -1,6 +1,8 @@
 class Supplement < ApplicationRecord
   belongs_to :user
   has_many :schedules, dependent: :destroy
+  has_many :record_supplements, dependent: :destroy
+  has_many :records, through: :record_supplements
 
   validates :supplement_name, presence: true
   validates :supplement_name, uniqueness: { scope: :user_id }
